21da62f643fea72f3892e1f0773aa09b8c0f9283
Zach Marmolejo <zachary.marmolejo@protonmail.com>
2021-04-30 08:17:39 -0500

    created test setup and tests for main, middleware and routes

---
+++ cmd/web/routes_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestRoutes(t *testing.T) {
+
+	mux := routes(&app)
+
+	switch v := mux.(type) {
+	case http.Handler:
+		// do nothing; test passed
+
+	default:
+		t.Error(fmt.Sprintf("type is %T", v))
+	}
+} // test fails... correctly.
