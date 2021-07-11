eafca02f960f48dd60e62e6bd1df2c25001e009f
Zach Marmolejo <zachary.marmolejo@protonmail.com>
2021-07-11 15:22:31 -0500

    added info log and error log

--- internal/config/config.go
+++ internal/config/config.go
@@ -12,6 +12,7 @@ type AppConfig struct {
 	UseCache      bool                          // sets value
 	TemplateCache map[string]*template.Template // creates a map[string] pointer to html/template package.Template structure
 	InfoLog       *log.Logger                   // logs relevant information as needed
+	ErrorLog      *log.Logger                   // logs relevant error information as needed
 	InProduction  bool                          // lets us set values T for when in production
 	Session       *scs.SessionManager           // allows for control of session length
 }
